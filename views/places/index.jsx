e83f42ba6a07b51f741e3acafe8736ed9e5be69f
LuckyShmucky <christianluciano567@gmail.com>
2022-03-14 17:34:40 -0700

    working on placement of elements in page for each place (index.jsx)

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -6,22 +6,27 @@ function index (data) {
     let placesFormatted = data.places.map((place, index) => {
       return (
         <div className='col-sm-6'>
-          <h2>{place.name}</h2>
-
+             
+              <h2>
           <a href={`/places/${index}`}>
-            {place.name}
+            {place.name}  
             </a>
+              </h2>
+         
+          <p className='' id='location'>Located in {place.city}, {place.state}</p>
           
-          <p className='text-center'>{place.cuisines}</p>
           
           <img src={place.pic} alt={place.name}></img>
+          <p className='' id='cuisines'>{place.cuisines}</p>
           
-          <p className='text-center'>Located in {place.city}, {place.state}</p>
         </div>
       )
     })
     return (
       <Def>
+        <head>
+          <link rel="stylesheet" href="../../index.css" />
+        </head>
           <main>
               <h1>Places to Rant or Rave About</h1>
               {placesFormatted}
