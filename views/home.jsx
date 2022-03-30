736e3c0559851e5237a50b4722b98a231702a529
LuckyShmucky <christianluciano567@gmail.com>
2022-03-30 16:05:55 -0700

    made changes to bttn placement and home

--- views/home.jsx
+++ views/home.jsx
@@ -4,17 +4,19 @@ const Def = require('./default')
 function home () {
     return (
       <Def>
+        <head>
+          <link rel="stylesheet" href="../home.css" />
+        </head>
           <main className='px-5'>
               <h1>REST-Rant</h1>
               <h2>This is a site where you can either complain (Rant) or compliment
                (Rave) a restaurant! 
               </h2>
-              <div>
-                <img src="/images/resizedCake.jpg" alt="A slice of cake with raspberries" />
-                  <div>
-                    Photo by <a href="https://unsplash.com/@anna_tukhfatullina">Anna Tukhfatullina</a>
-                    </div>            
-              </div>
+             <p>Thank you for visitting REST-rant! The name of the person who deployed this version of this site is Christian (Lucky) Luciano.
+                if you have any recommendations or questions about this site and how to make it better feel 
+                free to reach out to Lucky via <a href="https://www.linkedin.com/in/christian-luciano-a3b63621a/">Linkedin</a>
+            </p>
+             <a href=""></a>
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
